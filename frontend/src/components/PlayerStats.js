bed2f582217986ff502583e06394a115ac098b71
Pauli Partanen <pauli.tapio.partanen@outlook.com>
2021-03-26 09:22:54 +0200

    Add all stats to the graph with checkbox functionality

--- frontend/src/components/PlayerStats.js
+++ frontend/src/components/PlayerStats.js
@@ -234,6 +234,243 @@ const PlayerStats = (
         data: trailingMeanService.min(playerStatsFiltered),
         hidden: !minSelected
       },
+
+
+
+
+
+
+      {
+        label: 'pf',
+        data: playerStatsFiltered.map(playerStat => playerStat.pf),
+        fill: false,
+        borderColor: colors.yellowDot,
+        pointBackgroundColor: colors.yellowDot,
+        showLine: false,
+        hidden: !pfSelected
+      },
+      {
+        label: 'pf trailing mean',
+        fill: false,
+        borderColor: colors.yellowLine,
+        pointBackgroundColor: colors.yellowLine,
+        showLine: true,
+        pointRadius: 0,
+        data: trailingMeanService.pf(playerStatsFiltered),
+        hidden: !pfSelected
+      },
+      {
+        label: 'dreb',
+        data: playerStatsFiltered.map(playerStat => playerStat.dreb),
+        fill: false,
+        borderColor: colors.limeDot,
+        pointBackgroundColor: colors.limeDot,
+        showLine: false,
+        hidden: !drebSelected
+      },
+      {
+        label: 'dreb trailing mean',
+        fill: false,
+        borderColor: colors.limeLine,
+        pointBackgroundColor: colors.limeLine,
+        showLine: true,
+        pointRadius: 0,
+        data: trailingMeanService.dreb(playerStatsFiltered),
+        hidden: !drebSelected
+      },
+      {
+        label: 'oreb',
+        data: playerStatsFiltered.map(playerStat => playerStat.oreb),
+        fill: false,
+        borderColor: colors.greenDot,
+        pointBackgroundColor: colors.greenDot,
+        showLine: false,
+        hidden: !orebSelected
+      },
+      {
+        label: 'oreb trailing mean',
+        fill: false,
+        borderColor: colors.greenLine,
+        pointBackgroundColor: colors.greenLine,
+        showLine: true,
+        pointRadius: 0,
+        data: trailingMeanService.oreb(playerStatsFiltered),
+        hidden: !orebSelected
+      },
+      {
+        label: 'fg_pct',
+        data: playerStatsFiltered.map(playerStat => playerStat.fg_pct <= 1 ? playerStat.fg_pct * 100 : playerStat.fg_pct),
+        fill: false,
+        borderColor: colors.cyanDot,
+        pointBackgroundColor: colors.cyanDot,
+        showLine: false,
+        hidden: !fg_pctSelected
+      },
+      {
+        label: 'fg_pct trailing mean',
+        fill: false,
+        borderColor: colors.cyanLine,
+        pointBackgroundColor: colors.cyanLine,
+        showLine: true,
+        pointRadius: 0,
+        data: trailingMeanService.fg_pct(playerStatsFiltered
+          .map(playerStat => ({ ...playerStat, fg_pct: playerStat.fg_pct <= 1 ? playerStat.fg_pct * 100 : playerStat.fg_pct }))),
+        hidden: !fg_pctSelected
+      },
+      {
+        label: 'fg3_pct',
+        data: playerStatsFiltered.map(playerStat => playerStat.fg3_pct <= 1 ? playerStat.fg3_pct * 100 : playerStat.fg3_pct),
+        fill: false,
+        borderColor: colors.blueDot,
+        pointBackgroundColor: colors.blueDot,
+        showLine: false,
+        hidden: !fg3_pctSelected
+      },
+      {
+        label: 'fg3_pct trailing mean',
+        fill: false,
+        borderColor: colors.blueLine,
+        pointBackgroundColor: colors.blueLine,
+        showLine: true,
+        pointRadius: 0,
+        data: trailingMeanService.fg3_pct(playerStatsFiltered
+          .map(playerStat => ({ ...playerStat, fg3_pct: playerStat.fg3_pct <= 1 ? playerStat.fg3_pct * 100 : playerStat.fg3_pct }))),
+        hidden: !fg3_pctSelected
+      },
+      {
+        label: 'ft_pct',
+        data: playerStatsFiltered.map(playerStat => playerStat.ft_pct <= 1 ? playerStat.ft_pct * 100 : playerStat.ft_pct),
+        fill: false,
+        borderColor: colors.purpleDot,
+        pointBackgroundColor: colors.purpleDot,
+        showLine: false,
+        hidden: !ft_pctSelected
+      },
+      {
+        label: 'ft_pct trailing mean',
+        fill: false,
+        borderColor: colors.purpleLine,
+        pointBackgroundColor: colors.purpleLine,
+        showLine: true,
+        pointRadius: 0,
+        data: trailingMeanService.ft_pct(playerStatsFiltered
+          .map(playerStat => ({ ...playerStat, ft_pct: playerStat.ft_pct <= 1 ? playerStat.ft_pct * 100 : playerStat.ft_pct }))),
+        hidden: !ft_pctSelected
+      },
+      {
+        label: 'fga',
+        data: playerStatsFiltered.map(playerStat => playerStat.fga),
+        fill: false,
+        borderColor: colors.magentaDot,
+        pointBackgroundColor: colors.magentaDot,
+        showLine: false,
+        hidden: !fgaSelected
+      },
+      {
+        label: 'fga trailing mean',
+        fill: false,
+        borderColor: colors.magentaLine,
+        pointBackgroundColor: colors.magentaLine,
+        showLine: true,
+        pointRadius: 0,
+        data: trailingMeanService.fga(playerStatsFiltered),
+        hidden: !fgaSelected
+      },
+      {
+        label: 'fgm',
+        data: playerStatsFiltered.map(playerStat => playerStat.fgm),
+        fill: false,
+        borderColor: colors.greyDot,
+        pointBackgroundColor: colors.greyDot,
+        showLine: false,
+        hidden: !fgmSelected
+      },
+      {
+        label: 'fgm trailing mean',
+        fill: false,
+        borderColor: colors.greyLine,
+        pointBackgroundColor: colors.greyLine,
+        showLine: true,
+        pointRadius: 0,
+        data: trailingMeanService.fgm(playerStatsFiltered),
+        hidden: !fgmSelected
+      },
+      {
+        label: 'fg3a',
+        data: playerStatsFiltered.map(playerStat => playerStat.fg3a),
+        fill: false,
+        borderColor: colors.pinkDot,
+        pointBackgroundColor: colors.pinkDot,
+        showLine: false,
+        hidden: !fg3aSelected
+      },
+      {
+        label: 'fg3a trailing mean',
+        fill: false,
+        borderColor: colors.pinkLine,
+        pointBackgroundColor: colors.pinkLine,
+        showLine: true,
+        pointRadius: 0,
+        data: trailingMeanService.fg3a(playerStatsFiltered),
+        hidden: !fg3aSelected
+      },
+      {
+        label: 'fg3m',
+        data: playerStatsFiltered.map(playerStat => playerStat.fg3m),
+        fill: false,
+        borderColor: colors.apricotDot,
+        pointBackgroundColor: colors.apricotDot,
+        showLine: false,
+        hidden: !fg3mSelected
+      },
+      {
+        label: 'fg3m trailing mean',
+        fill: false,
+        borderColor: colors.apricotLine,
+        pointBackgroundColor: colors.apricotLine,
+        showLine: true,
+        pointRadius: 0,
+        data: trailingMeanService.fg3m(playerStatsFiltered),
+        hidden: !fg3mSelected
+      },
+      {
+        label: 'fta',
+        data: playerStatsFiltered.map(playerStat => playerStat.fta),
+        fill: false,
+        borderColor: colors.beigeDot,
+        pointBackgroundColor: colors.beigeDot,
+        showLine: false,
+        hidden: !ftaSelected
+      },
+      {
+        label: 'fta trailing mean',
+        fill: false,
+        borderColor: colors.beigeLine,
+        pointBackgroundColor: colors.beigeLine,
+        showLine: true,
+        pointRadius: 0,
+        data: trailingMeanService.fta(playerStatsFiltered),
+        hidden: !ftaSelected
+      },
+      {
+        label: 'ftm',
+        data: playerStatsFiltered.map(playerStat => playerStat.ftm),
+        fill: false,
+        borderColor: colors.mintDot,
+        pointBackgroundColor: colors.mintDot,
+        showLine: false,
+        hidden: !ftmSelected
+      },
+      {
+        label: 'ftm trailing mean',
+        fill: false,
+        borderColor: colors.mintLine,
+        pointBackgroundColor: colors.mintLine,
+        showLine: true,
+        pointRadius: 0,
+        data: trailingMeanService.ftm(playerStatsFiltered),
+        hidden: !ftmSelected
+      },
     ],
   }
   return (
--- frontend/src/services/trailingMeans.js
+++ frontend/src/services/trailingMeans.js
@@ -38,6 +38,64 @@ const min = (playerStats) => {
   playerStats = playerStats.map(playerStat => ({ ...playerStat, min: playerStat.min ? Number(playerStat.min.split(':')[0]) : null }))
   return countTrailingMean(playerStats, 'min')
 }
-const trailingMeanService = { pts, ast, reb, blk, stl, turnover, min }
+const pf = (playerStats) => {
+  return countTrailingMean(playerStats, 'pf')
+}
+const dreb = (playerStats) => {
+  return countTrailingMean(playerStats, 'dreb')
+}
+const oreb = (playerStats) => {
+  return countTrailingMean(playerStats, 'oreb')
+}
+const fg_pct = (playerStats) => {
+  return countTrailingMean(playerStats, 'fg_pct')
+}
+const fg3_pct = (playerStats) => {
+  return countTrailingMean(playerStats, 'fg3_pct')
+}
+const ft_pct = (playerStats) => {
+  return countTrailingMean(playerStats, 'ft_pct')
+}
+const fga = (playerStats) => {
+  return countTrailingMean(playerStats, 'fga')
+}
+const fgm = (playerStats) => {
+  return countTrailingMean(playerStats, 'fgm')
+}
+const fg3a = (playerStats) => {
+  return countTrailingMean(playerStats, 'fg3a')
+}
+const fg3m = (playerStats) => {
+  return countTrailingMean(playerStats, 'fg3m')
+}
+const fta = (playerStats) => {
+  return countTrailingMean(playerStats, 'fta')
+}
+const ftm = (playerStats) => {
+  return countTrailingMean(playerStats, 'ftm')
+}
+
+
+const trailingMeanService = {
+  pts,
+  ast,
+  reb,
+  blk,
+  stl,
+  turnover,
+  min,
+  pf,
+  dreb,
+  oreb,
+  fg_pct,
+  fg3_pct,
+  ft_pct,
+  fga,
+  fgm,
+  fg3a,
+  fg3m,
+  fta,
+  ftm
+}
 
 export default trailingMeanService
