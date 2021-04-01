b999354e79f71feb710abc7469e1942fbe512566
Pauli Partanen <pauli.tapio.partanen@outlook.com>
2021-04-01 10:14:04 +0300

    Install package colors and use it in the backend

--- controllers/stats.js
+++ controllers/stats.js
@@ -2,14 +2,10 @@ const statsRouter = require('express').Router()
 const Stat = require('../models/stat')
 const axios = require('axios')
 const baseUrl = 'https://www.balldontlie.io/api/v1/stats'
+const colors = require('colors')
 
 statsRouter.get('/statsfromapitodatabase', async (_req, _res) => {
 
-
-
-  //1-11298
-  //11298-11301
-  //11301-11311
   const getStats = async () => {
 
     const apiPerPage = 100
@@ -17,7 +13,6 @@ statsRouter.get('/statsfromapitodatabase', async (_req, _res) => {
     const documentsCountInDatabaseBeforeAdding = await Stat.count()
     console.log('documents in database before adding:', documentsCountInDatabaseBeforeAdding)
 
-    //let apiPageNumber = 11295
 
     let statsOnFirstPage = await axios.get(`${baseUrl}?per_page=${apiPerPage}`)
     const totalAmountDocumentsInApi = statsOnFirstPage.data.meta.total_count
@@ -28,7 +23,6 @@ statsRouter.get('/statsfromapitodatabase', async (_req, _res) => {
     let stats = []
 
     let apiPageNumber = startingPageNumber
-    //const total_pages = 11312
     while (apiPageNumber <= totalPages) {
       console.log(`getting stats, page ${apiPageNumber}`)
       let statsOnOnePage = await axios.get(`${baseUrl}?per_page=${apiPerPage}&page=${apiPageNumber}`)
@@ -40,14 +34,9 @@ statsRouter.get('/statsfromapitodatabase', async (_req, _res) => {
           const options = { upsert: true }
           const updateDoc = { $set: stat }
           await Stat.updateOne(filter, updateDoc, options)
-          //console.log('updated / added', savedStat, 'to database')
         }
         stats = []
         console.log('finished saving stats to database')
-        /* console.log('saving stats to database...')
-        await Stat.insertMany(stats)
-        stats = []
-        console.log('finished saving stats to database') */
       }
       apiPageNumber = apiPageNumber + 1
       //timer to prevent status code 429 (Too Many Requests)
@@ -56,23 +45,13 @@ statsRouter.get('/statsfromapitodatabase', async (_req, _res) => {
     const documentsCountInDatabaseAfterAdding = await Stat.count()
 
     if (documentsCountInDatabaseAfterAdding === totalAmountDocumentsInApi) {
-      console.log('Data successfully moved from API to database. Document count in API and database are the same')
+      console.log('Data successfully moved from API to database. Document count in API and database are the same.'.green)
     } else {
-      console.log('Error in moving data from API to database. Document count in API and database is not the same')
+      console.log('Error in moving data from API to database. Document count in API and database is not the same.'.red)
     }
-
     return stats
   }
-  const putStatsToDatabase = async (stats) => {
-    /* const newStats = stats.map(({ first_name: firstName, last_name: lastName, id: apiId, team: team }) =>
-      ({ firstName, lastName, apiId, team })) */
-    console.log('saving stats to database...')
-    await Stat.insertMany(stats)
-    console.log('finished saving stats to database')
-  }
   await getStats()
-  //const stats = await getStats()
-  //await putStatsToDatabase(stats)
 })
 
 module.exports = statsRouter
