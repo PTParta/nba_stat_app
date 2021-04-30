2ccba6e4f601eb28c293bd6b0db69e0ab9dceb4a
Pauli Partanen <pauli.tapio.partanen@outlook.com>
2021-04-30 14:10:07 +0300

    Change compare players season selection from 1979 to 1983

--- frontend/src/components/players/DescriptionPlayers.js
+++ frontend/src/components/players/DescriptionPlayers.js
@@ -5,6 +5,7 @@ const DescriptionPlayers = () => {
       <br></br>
       {/* <br></br> */}
       <p>This page shows a player's stats throughout his career.
+        Stats start from season 1983-1984.
         You can select the stats, seasons, regular or post season, and how many
         games are counted into the trailing average.
       </p>
