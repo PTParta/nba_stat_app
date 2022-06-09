d03d6f5566c917a2f19804cde49bf04ded5920ec
Pauli Partanen <pauli.tapio.partanen@outlook.com>
2022-06-09 18:42:07 +0300

    uibuild

--- frontend/src/components/contact/DescriptionContact.js
+++ frontend/src/components/contact/DescriptionContact.js
@@ -8,6 +8,12 @@ const DescriptionPlayers = () => {
         Here you can contact me. Please write your feedback, comments, questions etc.
         and click send. If you want you can give your email address for me to respond to.
       </p>
+      <br></br>
+      <p>
+        Ask and you shall receive :)
+      </p>
+      
+
     </div>
   )
 }
